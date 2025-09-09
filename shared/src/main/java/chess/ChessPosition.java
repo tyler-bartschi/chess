a88c4c9ef855cadfa13db3d4a3afbeb99d6a0ad5
tyler-bartschi <tylerabartschi@gmail.com>
2025-09-09 10:43:29 -0600

    added some more code for moves

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -35,5 +37,18 @@ public class ChessPosition {
     @Override
     public String toString() {
         return String.format("%d,%d", row, col);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ChessPosition that)) {
+            return false;
+        }
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
