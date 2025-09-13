9385baa39849f1c6baded4fd0473f0ab08a11749
tyler-bartschi <tylerabartschi@gmail.com>
2025-09-12 20:34:35 -0600

    bishop move tests works

--- shared/src/main/java/chess/movecalculators/BishopMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/BishopMoveCalculator.java
@@ -6,6 +6,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMoveCalculator {
+    private static boolean checkEnemyPresence(ChessBoard board, ChessPosition currentPosition, ChessPosition myPosition) {
+        ChessPiece occupant = board.getPiece(currentPosition);
+        ChessPiece myPiece = board.getPiece(myPosition);
+        return occupant != null && occupant.getTeamColor() != myPiece.getTeamColor();
+    }
+
+    private static boolean checkFriendlyPresence(ChessBoard board, ChessPosition currentPosition, ChessPosition myPosition) {
+        ChessPiece occupant = board.getPiece(currentPosition);
+        ChessPiece myPiece = board.getPiece(myPosition);
+        return occupant != null && occupant.getTeamColor() == myPiece.getTeamColor();
+    }
+
     public static Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         var possibleMoves = new ArrayList<ChessMove>();
         final int[][] steps = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
@@ -17,7 +29,13 @@ public class BishopMoveCalculator {
             int col = myPosition.getColumn() + colAdder;
 
             while (row <= 8 && col <= 8 && row > 0 && col > 0) {
+                if (checkFriendlyPresence(board, new ChessPosition(row, col), myPosition)) {
+                    break;
+                }
                 possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                if (checkEnemyPresence(board, new ChessPosition(row, col), myPosition)) {
+                    break;
+                }
                 row += rowAdder;
                 col += colAdder;
             }
