ae798b5a432b999c4dbf87fa4cc0b68c8bc6da97
tyler-bartschi <tylerabartschi@gmail.com>
2025-10-16 20:44:14 -0600

    updated existing tests

--- server/src/test/java/service/StandardServiceTests.java
+++ server/src/test/java/service/StandardServiceTests.java
@@ -1,6 +1,9 @@
 package service;
 
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import dataaccess.*;
 import server.exceptions.*;
 import service.requests.*;
@@ -22,7 +25,7 @@ public class StandardServiceTests {
         testDataAccess = new MemoryDataAccess();
         testUserService = new UserService(testDataAccess);
         RegisterRequest request = new RegisterRequest(START_USERNAME, START_EMAIL, START_PASSWORD);
-        Assertions.assertDoesNotThrow(() -> testUserService.register(request), "initial register in initialization fails");
+        assertDoesNotThrow(() -> testUserService.register(request), "initial register in initialization fails");
     }
 
 
@@ -31,10 +34,11 @@ public class StandardServiceTests {
     @DisplayName("Register Successful")
     public void registerSuccess() {
         RegisterRequest request = new RegisterRequest("myUser", "myEmail", "123");
-        RegisterResult result = Assertions.assertDoesNotThrow(() -> testUserService.register(request), "Initial register failed");
-        Assertions.assertNotNull(result, "Result was null");
-        Assertions.assertNotNull(result.authToken(), "Result did not contain an authToken");
-        Assertions.assertEquals(request.username(), result.username(), "Username of result does not match username of request");
+        RegisterResult result = assertDoesNotThrow(() -> testUserService.register(request), "Initial register failed");
+        assertNotNull(result, "Result was null");
+        assertInstanceOf(String.class, result.authToken(), "Result did not contain a valid authToken");
+        assertFalse(result.authToken().isEmpty(), "authToken is empty");
+        assertEquals(request.username(), result.username(), "Username of result does not match username of request");
     }
 
     @Test
@@ -42,7 +46,7 @@ public class StandardServiceTests {
     @DisplayName("Register Bad Request")
     public void registerBadReq() {
         RegisterRequest request = new RegisterRequest(START_USERNAME, "hasEmail", "2");
-        Assertions.assertThrows(AlreadyTakenException.class, () -> testUserService.register(request), "Does not throw a bad request exception");
+        assertThrows(AlreadyTakenException.class, () -> testUserService.register(request), "Does not throw a bad request exception");
     }
 
     @Test
@@ -50,10 +54,11 @@ public class StandardServiceTests {
     @DisplayName("Login Successful")
     public void loginSuccess() {
         LoginRequest request = new LoginRequest(START_USERNAME, START_PASSWORD);
-        LoginResult result = Assertions.assertDoesNotThrow(() -> testUserService.login(request), "Initial login failed");
-        Assertions.assertNotNull(result, "Result was null");
-        Assertions.assertNotNull(result.authToken(), "Result did not contain an authToken");
-        Assertions.assertEquals(request.username(), result.username(), "Username of result does not match username of request");
+        LoginResult result = assertDoesNotThrow(() -> testUserService.login(request), "Initial login failed");
+        assertNotNull(result, "Result was null");
+        assertInstanceOf(String.class, result.authToken(), "Result did not contain a valid authToken");
+        assertFalse(result.authToken().isEmpty(), "authToken is empty");
+        assertEquals(request.username(), result.username(), "Username of result does not match username of request");
 
     }
 
@@ -62,7 +67,7 @@ public class StandardServiceTests {
     @DisplayName("Login Bad Password")
     public void loginBadPass() {
         LoginRequest request = new LoginRequest(START_USERNAME, "notPassword");
-        Assertions.assertThrows(UnauthorizedException.class, () -> testUserService.login(request), "Does not throw an unauthorized exception for invalid password");
+        assertThrows(UnauthorizedException.class, () -> testUserService.login(request), "Does not throw an unauthorized exception for invalid password");
 
     }
 
@@ -71,7 +76,7 @@ public class StandardServiceTests {
     @DisplayName("Login Invalid Username")
     public void loginBadUser() {
         LoginRequest request = new LoginRequest("nonExistentUser", "123");
-        Assertions.assertThrows(UnauthorizedException.class, () -> testUserService.login(request), "Does not throw an unauthorized exception for invalid username");
+        assertThrows(UnauthorizedException.class, () -> testUserService.login(request), "Does not throw an unauthorized exception for invalid username");
     }
 
     @Test
@@ -81,7 +86,7 @@ public class StandardServiceTests {
         testUserService.clear();
         UserData noUser = testDataAccess.getUser(START_USERNAME);
         AuthData noAuth = testDataAccess.getAuth(START_USERNAME);
-        Assertions.assertNull(noUser, "Did not clear the user datatable");
-        Assertions.assertNull(noAuth, "Did not clear the auth datatable");
+        assertNull(noUser, "Did not clear the user datatable");
+        assertNull(noAuth, "Did not clear the auth datatable");
     }
 }
